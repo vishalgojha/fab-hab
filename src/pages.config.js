751284aaf4da041a0a77cda96e409a99c674ac65
Vishal Ojha <propai.live@outlook.com>
2025-12-12 10:18:52 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import Demo from './pages/Demo';
 
 
 export const PAGES = {
     "Home": Home,
     "Dashboard": Dashboard,
+    "Demo": Demo,
 }
 
 export const pagesConfig = {
